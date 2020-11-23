877b2b7bd9cac3f2441fc009cf66bed8f588550d
Daniel Abraham Braude <daniboy1007@gmail.com>
2020-11-23 23:53:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,4 +9,23 @@ class Dog
   def self.all
     @@all
   end
+
+  def self.clear_all
+   @@all = []
+ end
+
+ def self.print_all
+   @@all.each do |dog|
+     puts dog.name
+   end
+end
+
+ def save
+   @@all << self
+ end
+
+ def name
+   @name
+ end
+
 end
